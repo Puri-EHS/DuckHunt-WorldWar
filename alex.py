b4fb81528c2a50680aefff0f21fd1185178f04ae
Mihalcea-Alexander <alexmihalcea953@gmail.com>
2023-11-02 13:04:49 -0700

    added crouch ability

--- alex.py
+++ alex.py
@@ -2,6 +2,7 @@
 import pygame
 import sys
 from image_obj import image_object
+import time
 
 def depth_movement(obj_depth, move_amount):
     obj_move = move_amount/obj_depth
@@ -20,11 +21,21 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Image Move Example")
 move_speed = 5
 
+all_image_objects = []
+
 back_image = image_object("African-savannah-ecosystem.jpg", 2100,900,400,300,5)
-bush_image = image_object("dlf.pt-bush-png-29280.png", 2100, 450, 400, 550, 3)
+all_image_objects.append(back_image)
+
 backbush_image = image_object("dlf.pt-bush-png-29280.png", 2100, 200, 400, 300, 4)
+all_image_objects.append(backbush_image)
+
+bush_image = image_object("dlf.pt-bush-png-29280.png", 2100, 700, 400, 600, 2)
+all_image_objects.append(bush_image)
+
 # Main game loop
 running = True
+crouched = False
+jump = False
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -37,19 +48,27 @@ while running:
 
     # Update image position
     if keys[pygame.K_LEFT]:
-        back_image.image_rect.x += depth_movement(back_image.depth,move_speed)
-        bush_image.image_rect.x += depth_movement(bush_image.depth,move_speed)
-        backbush_image.image_rect.x += depth_movement(backbush_image.depth,move_speed)
+        for image in all_image_objects:
+            image.image_rect.x += depth_movement(image.depth, move_speed)
+       
     if keys[pygame.K_RIGHT]:
-        back_image.image_rect.x -= depth_movement(back_image.depth,move_speed)
-        bush_image.image_rect.x -= depth_movement(bush_image.depth,move_speed)
-        backbush_image.image_rect.x -= depth_movement(backbush_image.depth,move_speed)
+        for image in all_image_objects:
+            image.image_rect.x -= depth_movement(image.depth, move_speed)
     
+    if keys[pygame.K_DOWN] and crouched == False:
+        crouched = True
+        for image in all_image_objects:
+            image.image_rect.y -= depth_movement(image.depth, 500)
+    
+    if keys[pygame.K_UP] and crouched == True:
+        crouched = False
+        for image in all_image_objects:
+            image.image_rect.y += depth_movement(image.depth, 500)
+
+
     # Draw the image
-    
-    screen.blit(back_image.image, back_image.image_rect)
-    screen.blit(backbush_image.image, backbush_image.image_rect)
-    screen.blit(bush_image.image, bush_image.image_rect)
+    for image in all_image_objects:
+        screen.blit(image.image, image.image_rect)
     # Update the display
     pygame.display.flip()
 
