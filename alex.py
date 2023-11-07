f2f3b8dd22a1af0d2fb23c8778e080c841a537aa
Mihalcea-Alexander <alexmihalcea953@gmail.com>
2023-11-07 13:37:56 -0800

    improvments in ui

--- alex.py
+++ alex.py
@@ -5,8 +5,11 @@ from image_obj import image_object
 import time
 
 def depth_movement(obj_depth, move_amount):
-    obj_move = move_amount/obj_depth
-
+    if obj_depth != 0:
+        obj_move = move_amount/obj_depth
+    else:
+        obj_move = 0
+    
     return obj_move
 
 # Initialize Pygame
@@ -18,13 +21,16 @@ screen_height = 600
 
 # Create the screen
 screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption("Image Move Example")
-move_speed = 5
+pygame.display.set_caption("Image Move Test")
+move_speed = 8
 
 all_image_objects = []
 
-back_image = image_object("African-savannah-ecosystem.jpg", 2100,900,400,300,5)
+back_image = image_object("Duck Hunt Savanna-1.png.png", 1536,790,400,300,5)
 all_image_objects.append(back_image)
+
+duck_image = image_object("5a0193067ca233f48ba6272c.png", 300, 300, 400, 250, 4)
+all_image_objects.append(duck_image)
 
 backbush_image = image_object("dlf.pt-bush-png-29280.png", 2100, 200, 400, 300, 4)
 all_image_objects.append(backbush_image)
@@ -32,10 +38,19 @@ all_image_objects.append(backbush_image)
 bush_image = image_object("dlf.pt-bush-png-29280.png", 2100, 700, 400, 600, 2)
 all_image_objects.append(bush_image)
 
+bar_ui = image_object("bar.png", 0, 25, 750, 160, 0)
+ammo_ui = image_object("ammo4.png", 100, 200, 750, 75, 0)
+
 # Main game loop
 running = True
 crouched = False
 jump = False
+current_x = 0
+ammo = 4
+fired_cd = 0
+reload_time = 0
+reloading = False
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -47,29 +62,59 @@ while running:
     keys = pygame.key.get_pressed()
 
     # Update image position
-    if keys[pygame.K_LEFT]:
+    if keys[pygame.K_LEFT] and current_x <= 250:
+        current_x += depth_movement(5, move_speed)
         for image in all_image_objects:
             image.image_rect.x += depth_movement(image.depth, move_speed)
        
-    if keys[pygame.K_RIGHT]:
+    if keys[pygame.K_RIGHT] and current_x >= -250:
+        current_x -= depth_movement(5, move_speed)
         for image in all_image_objects:
             image.image_rect.x -= depth_movement(image.depth, move_speed)
     
     if keys[pygame.K_DOWN] and crouched == False:
         crouched = True
+        move_speed = 5
         for image in all_image_objects:
             image.image_rect.y -= depth_movement(image.depth, 500)
     
     if keys[pygame.K_UP] and crouched == True:
         crouched = False
+        move_speed = 8
         for image in all_image_objects:
             image.image_rect.y += depth_movement(image.depth, 500)
+    
+    if keys[pygame.K_SPACE] and ammo > 0 and fired_cd == 0:
+        fired_cd = 50
+        ammo -= 1
+        ammo_str = "ammo" + str(ammo) + ".png"
+        ammo_ui = image_object(ammo_str, 100, 200, 750, 75, 0)
 
+    if ammo == 0 and reloading == False:
+        reloading = True
+        reload_time = 150
+    elif ammo == 0:
+        reload_time -= 1
+
+    if ammo == 0 and reloading == True and reload_time == 0:
+        reloading = False
+        ammo = 4
+        ammo_ui = image_object("ammo4.png", 100, 200, 750, 75, 0)
+
+    if reloading:
+        bar_ui = image_object("bar.png", reload_time/2, 25, 750, 160, 0)
+    elif fired_cd > 0:
+        bar_ui = image_object("bar.png", fired_cd, 25, 750, 160, 0)
 
     # Draw the image
     for image in all_image_objects:
         screen.blit(image.image, image.image_rect)
     # Update the display
+    if fired_cd > 0:
+        fired_cd -= 1
+    
+    screen.blit(bar_ui.image, bar_ui.image_rect)
+    screen.blit(ammo_ui.image, ammo_ui.image_rect)
     pygame.display.flip()
 
 # Quit Pygame
