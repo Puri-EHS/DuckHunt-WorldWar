0d079872bc999f767773061ee466657042833020
Mihalcea-Alexander <alexmihalcea953@gmail.com>
2023-11-09 09:49:24 -0800

    made ankits gun system work with my 3d enviroment and my ui system. Also made bullets respond to user movement, so they look 3d, instead of "sticking" to the user.

--- alex.py
+++ alex.py
@@ -29,14 +29,17 @@ all_image_objects = []
 back_image = image_object("Duck Hunt Savanna-1.png.png", 1536,790,400,300,5)
 all_image_objects.append(back_image)
 
-duck_image = image_object("5a0193067ca233f48ba6272c.png", 300, 300, 400, 250, 4)
-all_image_objects.append(duck_image)
+##duck_image = image_object("5a0193067ca233f48ba6272c.png", 300, 300, 400, 250, 4)
+##all_image_objects.append(duck_image)
 
-backbush_image = image_object("dlf.pt-bush-png-29280.png", 2100, 200, 400, 300, 4)
+backbush_image = image_object("FrontShrubSavannah-1.png.png", 2100, 500, 400, 200, 4)
 all_image_objects.append(backbush_image)
 
-bush_image = image_object("dlf.pt-bush-png-29280.png", 2100, 700, 400, 600, 2)
+bush_image = image_object("FrontShrubSavannah-1.png.png", 2100, 700, 400, 315, 2)
 all_image_objects.append(bush_image)
+
+sec_bush_image = image_object("FrontShrubSavannah-1.png.png", 2100, 900, 400, 400, 2)
+all_image_objects.append(sec_bush_image)
 
 bar_ui = image_object("bar.png", 0, 25, 750, 160, 0)
 ammo_ui = image_object("ammo4.png", 100, 200, 750, 75, 0)
@@ -60,12 +63,12 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:
-                shooting = True
-        elif event.type == pygame.MOUSEBUTTONUP:
-            if event.button == 1:
-                shooting = False
+        ##if event.type == pygame.MOUSEBUTTONDOWN:
+            ##if event.button == 1:
+                ##shooting = True
+        ##elif event.type == pygame.MOUSEBUTTONUP:
+            ##if event.button == 1:
+                ##shooting = False
     # Clear the screen
     screen.fill((255, 255, 255))
 
@@ -76,30 +79,40 @@ while running:
         current_x += depth_movement(5, move_speed)
         for image in all_image_objects:
             image.image_rect.x += depth_movement(image.depth, move_speed)
-       
+        for bullet in gun.bullets:
+            bullet.posx += depth_movement(3, move_speed)
+
     if keys[pygame.K_RIGHT] and current_x >= -250:
         current_x -= depth_movement(5, move_speed)
         for image in all_image_objects:
             image.image_rect.x -= depth_movement(image.depth, move_speed)
+        for bullet in gun.bullets:
+            bullet.posx -= depth_movement(3, move_speed)
     
     if keys[pygame.K_DOWN] and crouched == False:
         crouched = True
         move_speed = 5
         for image in all_image_objects:
             image.image_rect.y -= depth_movement(image.depth, 500)
+        for bullet in gun.bullets:
+            bullet.posy -= depth_movement(3, 500)
     
     if keys[pygame.K_UP] and crouched == True:
         crouched = False
         move_speed = 8
         for image in all_image_objects:
             image.image_rect.y += depth_movement(image.depth, 500)
+        for bullet in gun.bullets:
+            bullet.posy += depth_movement(3, 500)
     
     if keys[pygame.K_SPACE] and ammo > 0 and fired_cd == 0:
+        shooting = True
         fired_cd = 50
         ammo -= 1
         ammo_str = "ammo" + str(ammo) + ".png"
         ammo_ui = image_object(ammo_str, 100, 200, 750, 75, 0)
-
+        
+   
     if ammo == 0 and reloading == False:
         reloading = True
         reload_time = 150
@@ -131,6 +144,7 @@ while running:
 
     if shooting:
         gun.shoot(pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1])
+        shooting = False
 
     gun.update_bullets(screen)
 
