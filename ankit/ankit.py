95757ce2bf1e0e5b602ac721923f164e9ce3e867
bredisrising <ankitmalhotra788@gmail.com>
2023-11-12 16:25:27 -0800

    crosshair and hand camera detection

--- ankit/ankit.py
+++ ankit/ankit.py
@@ -1,8 +1,13 @@
 import pygame
-import sys
 from image_obj import image_object
-import time
 from player_gun import PlayerGun
+import constants
+
+
+if not constants.USE_MOUSE:
+    import multiprocessing as mp
+    from gesture import get_points
+
 
 def depth_movement(obj_depth, move_amount):
     if obj_depth != 0:
@@ -12,144 +17,159 @@ def depth_movement(obj_depth, move_amount):
     
     return obj_move
 
-# Initialize Pygame
-pygame.init()
+def main(queue):
+    # Initialize Pygame
+    pygame.init()
 
-# Screen dimensions
-screen_width = 800
-screen_height = 600
+    # Screen dimensions
+    screen_width = 800
+    screen_height = 600
 
-# Create the screen
-screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption("Image Move Test")
-move_speed = 8
+    # Create the screen
+    screen = pygame.display.set_mode((screen_width, screen_height))
+    pygame.display.set_caption("Image Move Test")
+    move_speed = 8
 
-all_image_objects = []
+    all_image_objects = []
 
-back_image = image_object("../Images/Environments/Duck Hunt Savanna-1.png.png", 1536,790,400,300,5)
-all_image_objects.append(back_image)
+    back_image = image_object("../Images/Environments/Duck Hunt Savanna-1.png.png", 1536,790,400,300,5)
+    all_image_objects.append(back_image)
 
-##duck_image = image_object("5a0193067ca233f48ba6272c.png", 300, 300, 400, 250, 4)
-##all_image_objects.append(duck_image)
+    ##duck_image = image_object("5a0193067ca233f48ba6272c.png", 300, 300, 400, 250, 4)
+    ##all_image_objects.append(duck_image)
 
-backbush_image = image_object("../Images/Environments/SavannahShrubFront.png", 2100, 500, 400, 200, 4)
-all_image_objects.append(backbush_image)
+    backbush_image = image_object("../Images/Environments/SavannahShrubFront.png", 2100, 500, 400, 200, 4)
+    all_image_objects.append(backbush_image)
 
-bush_image = image_object("../Images/Environments/SavannahShrubFront.png", 2100, 700, 400, 315, 2)
-all_image_objects.append(bush_image)
+    bush_image = image_object("../Images/Environments/SavannahShrubFront.png", 2100, 700, 400, 315, 2)
+    all_image_objects.append(bush_image)
 
-sec_bush_image = image_object("../Images/Environments/SavannahShrubFront.png", 2100, 900, 400, 400, 2)
-all_image_objects.append(sec_bush_image)
+    sec_bush_image = image_object("../Images/Environments/SavannahShrubFront.png", 2100, 900, 400, 400, 2)
+    all_image_objects.append(sec_bush_image)
 
-bar_ui = image_object("../bar.png", 0, 25, 750, 160, 0)
-ammo_ui = image_object("../Images/Weapons/ammo4.png", 100, 200, 750, 75, 0)
+    bar_ui = image_object("../bar.png", 0, 25, 750, 160, 0)
+    ammo_ui = image_object("../Images/Weapons/ammo4.png", 100, 200, 750, 75, 0)
 
-# Main game loop
-running = True
-crouched = False
-jump = False
-current_x = 0
-ammo = 4
-fired_cd = 0
-reload_time = 0
-reloading = False
+    # Main game loop
+    running = True
+    crouched = False
+    jump = False
+    current_x = 0
+    ammo = 4
+    fired_cd = 0
+    reload_time = 0
+    reloading = False
 
 
-gun = PlayerGun()
+    gun = PlayerGun()
 
-shooting = False
+    shooting = False
 
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        ##if event.type == pygame.MOUSEBUTTONDOWN:
-            ##if event.button == 1:
-                ##shooting = True
-        ##elif event.type == pygame.MOUSEBUTTONUP:
-            ##if event.button == 1:
-                ##shooting = False
-    # Clear the screen
-    screen.fill((255, 255, 255))
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    running = False
+                    break
+            ##if event.type == pygame.MOUSEBUTTONDOWN:
+                ##if event.button == 1:
+                    ##shooting = True
+            ##elif event.type == pygame.MOUSEBUTTONUP:
+                ##if event.button == 1:
+                    ##shooting = False
+        # Clear the screen
+        screen.fill((255, 255, 255))
 
-    keys = pygame.key.get_pressed()
+        keys = pygame.key.get_pressed()
 
-    # Update image position
-    if keys[pygame.K_LEFT] and current_x <= 250:
-        current_x += depth_movement(5, move_speed)
-        for image in all_image_objects:
-            image.image_rect.x += depth_movement(image.depth, move_speed)
-        for bullet in gun.bullets:
-            bullet.posx += depth_movement(3, move_speed)
 
-    if keys[pygame.K_RIGHT] and current_x >= -250:
-        current_x -= depth_movement(5, move_speed)
-        for image in all_image_objects:
-            image.image_rect.x -= depth_movement(image.depth, move_speed)
-        for bullet in gun.bullets:
-            bullet.posx -= depth_movement(3, move_speed)
-    
-    if keys[pygame.K_DOWN] and crouched == False:
-        crouched = True
-        move_speed = 5
-        for image in all_image_objects:
-            image.image_rect.y -= depth_movement(image.depth, 500)
-        for bullet in gun.bullets:
-            bullet.posy -= depth_movement(3, 500)
-    
-    if keys[pygame.K_UP] and crouched == True:
-        crouched = False
-        move_speed = 8
-        for image in all_image_objects:
-            image.image_rect.y += depth_movement(image.depth, 500)
-        for bullet in gun.bullets:
-            bullet.posy += depth_movement(3, 500)
-    
-    if keys[pygame.K_SPACE] and ammo > 0 and fired_cd == 0:
-        shooting = True
-        fired_cd = 50
-        ammo -= 1
-        ammo_str = "../Images/Weapons/ammo" + str(ammo) + ".png"
-        ammo_ui = image_object(ammo_str, 100, 200, 750, 75, 0)
+        # Update image position
+        if keys[pygame.K_LEFT] and current_x <= 250:
+            current_x += depth_movement(5, move_speed)
+            for image in all_image_objects:
+                image.image_rect.x += depth_movement(image.depth, move_speed)
+            for bullet in gun.bullets:
+                bullet.posx += depth_movement(3, move_speed)
+
+        if keys[pygame.K_RIGHT] and current_x >= -250:
+            current_x -= depth_movement(5, move_speed)
+            for image in all_image_objects:
+                image.image_rect.x -= depth_movement(image.depth, move_speed)
+            for bullet in gun.bullets:
+                bullet.posx -= depth_movement(3, move_speed)
         
-   
-    if ammo == 0 and reloading == False:
-        reloading = True
-        reload_time = 150
-    elif ammo == 0:
-        reload_time -= 1
-
-    if ammo == 0 and reloading == True and reload_time == 0:
-        reloading = False
-        ammo = 4
-        ammo_ui = image_object("../Images/Weapons/ammo4.png", 100, 200, 750, 75, 0)
-
-    if reloading:
-        bar_ui = image_object("../bar.png", reload_time/2, 25, 750, 160, 0)
-    elif fired_cd > 0:
-        bar_ui = image_object("../bar.png", fired_cd, 25, 750, 160, 0)
-
-    # Draw the image
-    for image in all_image_objects:
-        screen.blit(image.image, image.image_rect)
-    # Update the display
-    if fired_cd > 0:
-        fired_cd -= 1
+        if keys[pygame.K_DOWN] and crouched == False:
+            crouched = True
+            move_speed = 5
+            for image in all_image_objects:
+                image.image_rect.y -= depth_movement(image.depth, 500)
+            for bullet in gun.bullets:
+                bullet.posy -= depth_movement(3, 500)
+        
+        if keys[pygame.K_UP] and crouched == True:
+            crouched = False
+            move_speed = 8
+            for image in all_image_objects:
+                image.image_rect.y += depth_movement(image.depth, 500)
+            for bullet in gun.bullets:
+                bullet.posy += depth_movement(3, 500)
+        
+        if keys[pygame.K_SPACE] and ammo > 0 and fired_cd == 0:
+            shooting = True
+            fired_cd = 50
+            ammo -= 1
+            ammo_str = "../Images/Weapons/ammo" + str(ammo) + ".png"
+            ammo_ui = image_object(ammo_str, 100, 200, 750, 75, 0)
+            
     
-    screen.blit(bar_ui.image, bar_ui.image_rect)
-    screen.blit(ammo_ui.image, ammo_ui.image_rect)
+        if ammo == 0 and reloading == False:
+            reloading = True
+            reload_time = 150
+        elif ammo == 0:
+            reload_time -= 1
 
-    gun.update ()
-    gun.render(screen)
+        if ammo == 0 and reloading == True and reload_time == 0:
+            reloading = False
+            ammo = 4
+            ammo_ui = image_object("../Images/Weapons/ammo4.png", 100, 200, 750, 75, 0)
 
-    if shooting:
-        gun.shoot(pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1])
-        shooting = False
+        if reloading:
+            bar_ui = image_object("../bar.png", reload_time/2, 25, 750, 160, 0)
+        elif fired_cd > 0:
+            bar_ui = image_object("../bar.png", fired_cd, 25, 750, 160, 0)
 
-    gun.update_bullets(screen)
+        # Draw the image
+        for image in all_image_objects:
+            screen.blit(image.image, image.image_rect)
+        # Update the display
+        if fired_cd > 0:
+            fired_cd -= 1
+        
+        screen.blit(bar_ui.image, bar_ui.image_rect)
+        screen.blit(ammo_ui.image, ammo_ui.image_rect)
 
-    pygame.display.flip()
+        gun.update(queue)
+        gun.render(screen)
 
-# Quit Pygame
-pygame.quit()
-sys.exit()
+        if shooting:
+            gun.shoot()
+            shooting = False
+
+        gun.update_bullets(screen)
+
+        pygame.display.flip()
+
+
+if __name__ == '__main__':
+    if not constants.USE_MOUSE:
+        q = mp.Queue()
+        print("starting duck hunt...")
+        p1 = mp.Process(target=get_points, args=(q,))
+        p1.start()
+
+        main(q)
+    else:
+        main(None)
+
+    if not constants.USE_MOUSE:
+        p1.kill()
--- ankit/player_gun.py
+++ ankit/player_gun.py
@@ -2,11 +2,23 @@ import pygame
 import math
 import numpy as np
 from Spritesheet import Spritesheet
+import constants
 
 class PlayerGun:
     def __init__(self):
 
-        self.crosshair = None
+        self.crosshair = pygame.image.load("./crosshair.png").convert_alpha()
+        self.crosshair_coords = (400, 300)
+        self.cross_surf = pygame.Surface((self.crosshair.get_width(), self.crosshair.get_height()), pygame.SRCALPHA)
+        self.cross_surf.blit(self.crosshair, (0,0))
+        self.cross_surf = pygame.transform.scale(self.cross_surf, (30, 30))
+
+
+        if not constants.USE_MOUSE:
+            self.prev_crosshair_coords = self.crosshair_coords
+
+        pygame.mouse.set_visible(False)
+        #pygame.mouse.set_cursor(pygame.cursor.Cursor(self.cross_surf))
 
         self.idle_images = []
         self.shoot_images= []
@@ -29,6 +41,15 @@ class PlayerGun:
         self.gun_image_index = 1
         self.did_just_shoot = False
 
+        self.can_shoot = True
+
+        self.reload_frame_time = 10
+        self.shoot_frame_time = 10
+        self.shoot_frames = 0
+
+        self.max_ammo = 4
+        self.ammo_left = self.max_ammo
+
 
         self.cur_image = self.idle_images[1]
 
@@ -40,83 +61,54 @@ class PlayerGun:
 
         self.angle = 0
 
-        self.frames = 0
 
-    def update(self):
-        if pygame.mouse.get_pos()[0] < 800/3.0:
+    def update(self, queue):
+        if not constants.USE_MOUSE:
+            if not queue.empty():
+                tip, nuckle = queue.get()
+                print(tip)
+
+                if tip is not None:
+                    if not self.did_just_shoot:
+                        if self.crosshair_coords[1] - tip.y * 600 > 50:
+                            self.shoot()
+                        else:
+                            self.prev_crosshair_coords = self.crosshair_coords
+                            self.crosshair_coords = (800 - tip.x * 800, tip.y * 600)
+                    elif self.shoot_frames > self.shoot_frame_time:
+                        self.prev_crosshair_coords = self.crosshair_coords
+                        self.crosshair_coords = (800 - tip.x * 800, tip.y * 600)
+        else:
+            self.crosshair_coords = pygame.mouse.get_pos()
+
+
+        if self.crosshair_coords[0] < 800/3.0:
             self.gun_image_index = 0
-        elif pygame.mouse.get_pos()[0] > 800/3.0 and pygame.mouse.get_pos()[0] < 800/3.0 * 2:
+        elif self.crosshair_coords[0] > 800/3.0 and self.crosshair_coords[0] < 800/3.0 * 2:
             self.gun_image_index = 1
         else:
             self.gun_image_index = 2
 
-        # print(self.gun_image_index)
 
-        #self.cur_image = self.idle_images[self.gun_image_index]
-
-        self.frames += 1
-        if(self.did_just_shoot and self.frames > 10):
+        self.shoot_frames += 1
+        if(self.did_just_shoot and self.shoot_frames > self.shoot_frame_time):
             self.did_just_shoot = False
         elif not self.did_just_shoot:
             self.cur_image = self.idle_images[self.gun_image_index]
 
-        # self.angle = abs((pygame.mouse.get_pos()[0]) / 800 * math.pi - math.pi)
 
     def render(self, screen):
         screen.blit(self.cur_image, (800/2-self.size[0]/2, 600-self.size[1]))
-
+        screen.blit(self.cross_surf, self.crosshair_coords)
 
     def update_bullets(self, screen):
         for bullet in self.bullets:
             bullet.move()
             bullet.render(screen)
 
-
-    def shoot(self, mousex, mousey):
-        # velocity = np.array([(mousex - 400) / 800, (mousey - 600) / 600, 1])
-        # bullet = Bullet(velocity[0] / self.fov, velocity[1] / self.fov, velocity[2], mousex, mousey, 0, 0)
-        
-        # #bullet = Bullet(velocity[0] * 5, velocity[1] * 5, velocity[2], mousex, mousey, 0, 0)
-        # self.bullets.append(bullet)
-
-
+    def shoot(self):
         self.cur_image = self.shoot_images[self.gun_image_index]
         self.did_just_shoot = True
-        self.frames = 0
+        self.shoot_frames = 0
 
 
-class Bullet:
-
-    def __init__(self, velx, vely, velz, initx, inity, bullet_drop, bullet_slowdown):
-        self.dirx = velx
-        self.diry = vely
-        self.dirz = velz
-
-        self.posx = initx
-        self.posy = inity
-        self.posz = .05
-
-        self.speed = .025
-
-        self.dirx *= self.speed
-        self.diry *= self.speed
-        self.dirz *= self.speed
-
-        self.angle = 0
-
-        self.bullet_drop = .015
-
-
-    def move(self):
-        self.diry += self.bullet_drop
-        self.posx += self.dirx
-        self.posy += self.diry 
-        self.posz += self.dirz 
-
-    def render(self, screen):
-        pygame.draw.circle(screen, (0,0,0), (self.posx, self.posy), 5.0 / self.posz)
-
-
-    
-
-        
